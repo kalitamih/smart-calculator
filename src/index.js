7ecb5ab316c15db55b2ed20d97a3f7f1c497ae95
Kalita Mikhail <mkalita1991@gmail.com>
2018-09-25 19:50:03 +0300

    11th

--- src/index.js
+++ src/index.js
@@ -1,27 +1,58 @@
-class SmartCalculator {
-  constructor(initialValue) {
-    // your implementation
-  }
+class SmartCalculator { 
 
-  add(number) {
-    // your implementation
-  }
+  constructor(initialValue) { 
+    this.array = []; 
+    this.array[0] = initialValue; 
+    this.counter = 1; 
+
+    SmartCalculator.prototype.toString = function SmartCalculatorToString() { 
+          let stringForInputExpression = ''; 
+          
+      while ( this.array.lastIndexOf("^") != -1 ) { 
+        this.array.splice( this.array.lastIndexOf("^") - 1 , 3, Math.pow( this.array[this.array.lastIndexOf("^") - 1], 
+                                   this.array[this.array.lastIndexOf("^") + 1] ) ); 
+          } 
+      
+      for (let i=0; i < this.array.length; i++) 
+        stringForInputExpression = stringForInputExpression + this.array[i]; 
   
-  subtract(number) {
-    // your implementation
-  }
+    return eval(stringForInputExpression); 
+    } 
+  } 
 
-  multiply(number) {
-    // your implementation
-  }
+  add(number) { 
+    this.array[this.counter++] = "+"; 
+    this.array[this.counter++] = number; 
+    
+    return this; 
+    } 
+  
+  subtract(number) { 
+    this.array[this.counter++] = "-"; 
+    this.array[this.counter++] = number; 
+    
+    return this;         
+    } 
 
-  devide(number) {
-    // your implementation
-  }
+  multiply(number) { 
+    this.array[this.counter++] = "*"; 
+    this.array[this.counter++] = number; 
+  
+    return this; 
+    } 
 
-  pow(number) {
-    // your implementation
+  devide(number) { 
+    this.array[this.counter++] = "/"; 
+    this.array[this.counter++] = number; 
+    return this; 
+    } 
+
+  pow(number) { 
+    this.array[this.counter++] = "^"; 
+    this.array[this.counter++] = number; 
+    
+    return this; 
+    } 
   }
-}
 
 module.exports = SmartCalculator;
